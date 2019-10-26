dee3b1418c54a729a9b2017bcb307a11532ad3db
Olga  Chupp <olga.chupp@gmail.com>
2019-10-26 00:43:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,7 +25,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  matrix_lookup(assembled_matrix, 1, 1)
+  matrix_lookup(assembled_matrix, 1, 1)=
 end
 
 def matrix_update(matrix, row, column, new_value)
